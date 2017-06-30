8e8f1cbd2aebae65b6c2f253f444cad2ed554c0a
Adrian Hall <adrian@shellmonger.com>
2017-06-30 13:21:20 -0700

    Added MultiDex application code

---
+++ app/src/main/java/com/shellmonger/notes/Application.java
@@ -0,0 +1,15 @@
+package com.shellmonger.notes;
+
+import android.support.multidex.MultiDexApplication;
+
+
+/**
+ * Application class responsible for initializing singletons and other
+ * common components
+ */
+public class Application extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
